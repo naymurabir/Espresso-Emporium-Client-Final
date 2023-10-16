dfeb57eaafe6440e9f7980a518e9bd893dc9b54d
naymurabir <naymurrahman.abir@gmail.com>
2023-10-17 05:14:32 +0600

    Footer

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Navbar/Navbar";
+import Footer from "../Pages/Footer/Footer";
 
 const Root = () => {
     return (
         <div className="max-w-screen-xl mx-auto px-2 md:px-12 lg:px-20 font-rancho">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/Pages/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer footer-center p-4 bg-[#372727] text-white">
+            <aside>
+                <p>Copyright © 2023 - All right reserved by Naymur Rahman Abir</p>
+            </aside>
+        </footer>
+
+    );
+};
+
+export default Footer;
